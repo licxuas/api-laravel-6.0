b59cdac8ef42eae4fce2a3c202fb2d37d625e289
long <510865496@qq.com>
2019-10-14 02:57:35 +0000

    install cors

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Dingo\Api\Routing\Helpers;
 
-class Controller extends App\Http\Controllers\Controller
+class Controller extends \App\Http\Controllers\Controller
 {
     use Helpers;
 }
